2ca55a0044d509d53c962abc397002e3691a2580
viniciussilva2504 <vinicius.silva2504@gmail.com>
2025-02-09 12:54:44 +0000

    setup final para vercel

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,19 @@
 AOS.init();
 
-const dataDoEvento = new Date("Dec 12, 2022 19:00:00");
+const dataDoEvento = new Date("Dec 12, 2025 19:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
 const contaAsHoras = setInterval(function() {
     const agora = new Date();
-    const timeStampAtual = agora.getTime();
+    const timeStampAtual = agora.getTime(); //recupera pela data atual
 
     const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById("contador").innerHTML = "O evento já ocorreu";
+        return;
+    }
 
     const diaEmMs = 1000 * 60 * 60 * 24;
     const horaEmMs = 1000 * 60 * 60;
@@ -18,10 +24,5 @@ const contaAsHoras = setInterval(function() {
     const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
     const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
 
-    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
-
-    if (distanciaAteOEvento < 0) {
-        clearInterval(contaAsHoras);
-        document.getElementById('contador').innerHTML = 'Evento expirado';
-    }
+    document.getElementById("contador").innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}min ${segundosAteOEvento}s`;
 }, 1000);
